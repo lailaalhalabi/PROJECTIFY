fd39fc3f6c5d89f6fd5b1edf870754413f74b0b9
Quang Tran <nhatquang304@gmail.com>
2022-04-07 21:14:24 -0400

    updated custom font config

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,7 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 const SignIn = () => {
+  // Load custom fonts
+  const [fontsLoaded] = useFonts({
+    PoppinsBold: require("../assets/fonts/Poppins-Bold.ttf"),
+    PoppinsMedium: require("../assets/fonts/Poppins-Medium.ttf"),
+    PoppinsRegular: require("../assets/fonts/Poppins-Regular.ttf"),
+    Varela: require("../assets/fonts/Varela-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <View>
       <Text style={theme.yourClassName}>SignIn screen</Text>
@@ -16,5 +29,6 @@ const theme = StyleSheet.create({
   yourClassName: {
     fontSize: 50,
     fontWeight: "bold",
+    fontFamily: "PoppinsBold",
   },
 });
